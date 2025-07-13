cda612736d65a93e69d46eba776c13b208632f65
Loic CHECK <ndzieloic334@gmail.com>
2025-07-13 19:03:55 +0100

    first day

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Lalakipo Blog Post Page</h1>
+      <p>Lalakipo blogging is fun</p>
+    </>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Lalakipo blog</h1>
+      <p>This is a blog</p>
+    </>
+  );
+}
